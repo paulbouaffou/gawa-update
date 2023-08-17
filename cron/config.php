02c6516b0c3ac2f71fbe42981341604a9b2c22d3
Samuel Guebo <samuelguebo@gmail.com>
2023-08-17 15:55:58 -0600

    Fix absolute path in config

--- cron/config.php
+++ cron/config.php
@@ -14,7 +14,7 @@
 */
 
 
-$config = parse_ini_file(realpath("../.env"));
+$config = parse_ini_file(realpath(__DIR__. "/../.env"));
 
 $db_connection = "mysql:host=" . $config['DB_HOST'] . ";dbname=" . $config['DB_DATABASE'] . ";charset=utf8";
 
